c8c994ec181d0b8571ad50aa9a392bb2190a396f
ThomasWTH <Thomas139723@gmail.com>
2025-09-04 22:57:28 +0800

    allows input of range

--- RNG.py
+++ RNG.py
@@ -4,6 +4,6 @@ from random import randint
 low = int(input('Lower limit >> '))
 up = int(input('Upper limit >> '))
 
-answer = random.randint(low, up)
-
-print(answer)
+for i in range(100):
+    answer = random.randint(low, up)
+    print(answer)
